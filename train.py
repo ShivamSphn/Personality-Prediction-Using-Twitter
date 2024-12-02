9e37b542b8254e37d6fb551d7e90f4922cb7569f
shivam bansal <Shivam@spheron.network>
2024-12-02 15:01:50 +0530

    a

---
+++ train.py
@@ -0,0 +1,537 @@
+# %%
+
+
+# %%
+from google.colab import drive
+drive.mount('/content/drive')
+
+# %%
+ROOT = "/content/drive/MyDrive/myers-briggs-tweet-classification-main/"
+DATA = ROOT+'dataset/mb_data.csv'
+MODEL = ROOT+'saved-models/'
+
+# %% [markdown]
+# # Myers-Briggs Personality Classification with ML algorithms
+
+# %% [markdown]
+# #### This notebook includes following steps:
+# * Exploratory Data Analysis
+# * Data Cleaning
+# * Data Preprocessing
+# * Model Creation
+# * Model Training
+# * Model Evaluation and Selection
+# 
+
+# %% [markdown]
+# ### Importing Libraries
+
+# %%
+!pip install contractions
+
+# %%
+!pip install nltk==3.6.2
+
+# %%
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import re
+import nltk
+from nltk import FreqDist
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords, wordnet
+from nltk.stem import WordNetLemmatizer
+from wordcloud import WordCloud
+from collections import Counter
+from itertools import chain
+import contractions
+from nltk.collocations import BigramAssocMeasures, BigramCollocationFinder
+from nltk.collocations import TrigramAssocMeasures, TrigramCollocationFinder
+from nltk import ngrams
+
+# %% [markdown]
+# ### Load Dataset
+
+# %%
+data = pd.read_csv(ROOT+"dataset/mb_data.csv")
+data.head()
+
+# %% [markdown]
+# #### some information about dataset
+
+# %%
+data.info()
+
+# %%
+data.describe()
+
+# %% [markdown]
+# ### Class Distribution
+
+# %%
+_classes = data.type.unique()
+print(_classes)
+
+# %%
+def show_class_distribution(data, x="type", figsize=(16,4), title="Distribution of Personality Types", xticks_size=10, palette="husl"):
+    plt.figure(figsize=figsize)
+    sns.countplot(x=x, data=data, palette=palette)
+    plt.xlabel("Personality Types", size=15)
+    plt.ylabel("Counts", size=15)
+    plt.xticks(size=xticks_size)
+    plt.title(title, size=20)
+    plt.show()
+
+# %%
+show_class_distribution(data, xticks_size=14)
+
+# %%
+def divide_types(df):
+    df["E-I"] = ""
+    df["N-S"] = ""
+    df["F-T"] = ""
+    df["J-P"] = ""
+    for index, row in df.iterrows():
+        row["E-I"] = "E" if row.type[0] == "E" else "I"
+        row["N-S"] = "N" if row.type[1] == "N" else "S"
+        row["F-T"] = "F" if row.type[2] == "F" else "T"
+        row["J-P"] = "J" if row.type[3] == "J" else "P"
+    return df
+
+data = divide_types(data)
+
+# %%
+show_class_distribution(data, x="E-I", title="Distribution of I & E", figsize=(9,3), xticks_size=20, palette="icefire")
+
+# %%
+show_class_distribution(data, x="N-S", title="Distribution of N & S", figsize=(9,3), xticks_size=20, palette="cubehelix")
+
+# %%
+show_class_distribution(data, x="F-T", title="Distribution of F & T", figsize=(9,3), xticks_size=20, palette="viridis")
+
+# %%
+show_class_distribution(data, x="J-P", title="Distribution of J & P", figsize=(9,3), xticks_size=20, palette="flare")
+
+# %% [markdown]
+# #### sample post
+
+# %%
+data.loc[7, "posts"]
+
+# %% [markdown]
+# ### Cleaning
+
+# %%
+def fix_contractions(df, column_name = "posts", new_column="cleaned_post"):
+    df[new_column] = df[column_name].apply(lambda x: contractions.fix(x))
+    return df
+
+data = fix_contractions(data)
+
+# %%
+def clean_data(df, column_name = "cleaned_post"):
+    df[column_name] = df[column_name].apply(lambda x: x.lower())
+    df[column_name] = df[column_name].apply(lambda x: re.sub(r'@([a-zA-Z0-9_]{1,50})', '', x))
+    df[column_name] = df[column_name].apply(lambda x: re.sub(r'#([a-zA-Z0-9_]{1,50})', '', x))
+    df[column_name] = df[column_name].apply(lambda x: re.sub(r'http[s]?://\S+', '', x))
+    df[column_name] = df[column_name].apply(lambda x: re.sub(r'[^A-Za-z]+', ' ', x))
+    df[column_name] = df[column_name].apply(lambda x: re.sub(r' +', ' ', x))
+    df[column_name] = df[column_name].apply(lambda x: " ".join([word for word in x.split() if not len(word) <3]))
+    return df
+
+data = clean_data(data)
+
+# %% [markdown]
+# #### cleaned post
+
+# %%
+data.loc[7,"cleaned_post"]
+
+# %%
+data["words_count"] = data["cleaned_post"].apply(lambda x: len(x.split()))
+data.head(5)
+
+# %%
+def plot_counts(df, column, xlabel):
+    fig = plt.figure()
+    plt.xlabel(xlabel)
+    plt.ylabel("Frequency")
+    df[column].plot.hist(bins=25)
+
+# %%
+plot_counts(data, column="words_count", xlabel="Words Count")
+
+# %%
+data["char_count"] = data["cleaned_post"].apply(lambda x: len(x))
+data.head(5)
+
+# %%
+plot_counts(data, column="char_count", xlabel="Character Count")
+
+# %% [markdown]
+# ### Most Frequent Words
+
+# %%
+import nltk
+nltk.download('stopwords')
+
+# %%
+stopword_list = stopwords.words("english")
+
+# %%
+def get_most_frequent(data, stop_words, column="cleaned_post", top=25):
+    df = data[column].apply(lambda x: " ".join([word for word in x.split() if not word in stop_words]))
+    counter = Counter(" ".join(df).split())
+    return counter.most_common(top)
+
+# %%
+most_frequents = get_most_frequent(data, stopword_list)
+most_frequents[:10]
+
+# %%
+def show_most_frequents(most_frequent_words, top=20):
+    most_frequent_df = pd.DataFrame(most_frequent_words)    
+    plt.figure(figsize=(16,4))
+    my_cmap = plt.get_cmap("viridis")
+    plt.bar(x=most_frequent_df.iloc[:top, 0], height=most_frequent_df.iloc[:top, 1], color="slateblue")
+    plt.xlabel("Words", size=17)
+    plt.ylabel("Counts", size=17)
+    plt.title("Most Frequent Words", size = 20)
+    plt.show()
+
+# %%
+show_most_frequents(most_frequents)
+
+# %% [markdown]
+# ### WordClouds
+
+# %%
+def show_wordcloud(data, stopword_list, column="cleaned_post"):
+    fig = plt.figure(figsize=(15,5))
+    wordcloud = WordCloud(background_color="black", min_font_size=5, stopwords=stopword_list).generate(data[column].to_string())
+    plt.axis("off")
+    plt.imshow(wordcloud)
+    plt.show()
+
+# %%
+show_wordcloud(data, stopword_list)
+
+# %%
+def show_sub_wordclouds(data, type_column, column, size, fig_size=(20,15)):
+    classes = data[type_column].unique()
+    fig, ax = plt.subplots(len(classes), figsize=fig_size)
+    j = 0
+    for _class in classes:
+        temp = data[data[type_column] == _class]
+        wordcloud = WordCloud(background_color="black").generate(temp[column].to_string())
+        plt.subplot(*size, j+1)
+        plt.title(_class, size=25)
+        plt.imshow(wordcloud)
+        plt.axis("off")
+        j+=1
+
+# %%
+show_sub_wordclouds(data, type_column="type" , column="cleaned_post", size=(4,4))
+
+# %%
+show_sub_wordclouds(data, type_column="E-I" , column="cleaned_post", size=(1,2), fig_size=(16,8))
+
+# %%
+show_sub_wordclouds(data, type_column="N-S" , column="cleaned_post", size=(1,2), fig_size=(16,8))
+
+# %%
+show_sub_wordclouds(data, type_column="F-T" , column="cleaned_post", size=(1,2), fig_size=(16,8))
+
+# %%
+show_sub_wordclouds(data, type_column="J-P" , column="cleaned_post", size=(1,2), fig_size=(16,8))
+
+# %% [markdown]
+# ### N-Grams
+
+# %%
+def get_ngrams(data, n_gram, new_column, column="cleaned_post"):
+    data["tokenized"]  = data[column].apply(lambda x: x.split())
+    data["sw_removal"] = data["tokenized"].apply(lambda x: [y for y in x if not y in stopword_list])
+    data[new_column]   = data["sw_removal"].apply(lambda x: list(ngrams(x, n_gram)))
+    data.drop(columns  = ["tokenized", "sw_removal"], inplace=True)
+    return data
+
+# %%
+data = get_ngrams(data, n_gram=2, new_column="bigrams")
+data.head()
+
+# %%
+data = get_ngrams(data, n_gram=3, new_column="trigrams")
+data.head()
+
+# %%
+def most_common_ngram(data, column, top=20):
+    temp = []
+    for index, row in data.iterrows():
+        temp += row[column]
+    most_common = Counter(temp).most_common(top)
+    return most_common
+
+# %%
+def plot_n_grams(ngrams, title, top=20):
+    ngram_df = pd.DataFrame(ngrams)
+    ngram_df.iloc[:, 0] = ngram_df.iloc[:,0].astype(str)
+    plt.figure(figsize=(7,7))
+    plt.barh(y=ngram_df.iloc[:top, 0], width=ngram_df.iloc[:top, 1])
+    plt.xlabel("Counts", size=17)
+    plt.ylabel("Pairs", size=17)
+    plt.title(title, size = 20)
+    plt.show() 
+
+# %%
+bigrams_most_common = most_common_ngram(data, "bigrams")
+bigrams_most_common
+
+# %%
+plot_n_grams(bigrams_most_common, title="Most Frequent Bigrams")
+
+# %%
+trigrams_most_common = most_common_ngram(data, "trigrams")
+trigrams_most_common
+
+# %%
+plot_n_grams(trigrams_most_common, title="Most Frequent Trigrams")
+
+# %% [markdown]
+# ### preprocessing
+
+# %%
+nltk.download('wordnet')
+
+# %%
+def remove_stopwords(data, stopword_list, column="cleaned_post"):
+    data[column] = data[column].apply(word_tokenize)
+    data[column] = data[column].apply(lambda x: [word for word in x if not word in stopword_list])
+    return data
+
+# %%
+def apply_lemmatization(text):
+    lemmatizer = WordNetLemmatizer()
+    return [lemmatizer.lemmatize(w) for w in text]
+
+# %%
+def lemmatize(data, stopword_list, column="cleaned_post"):
+    data[column] = data[column].apply(apply_lemmatization)
+    data[column] = data[column].apply(" ".join)
+    return data
+
+# %%
+nltk.download('punkt')
+
+# %%
+data = remove_stopwords(data, stopword_list)
+
+# %%
+nltk.download('wordnet')
+
+# %%
+data = lemmatize(data, stopword_list)
+
+# %%
+data.head()
+
+# %%
+training_data = data[["cleaned_post", "E-I", "N-S", "F-T", "J-P"]].copy()
+training_data.head(5)
+
+# %%
+def make_dummies(data, columns=["E-I", "N-S", "F-T", "J-P"]):
+    for column in columns:
+        temp_dummy = pd.get_dummies(data[column], prefix="type")
+        data = data.join(temp_dummy)
+    return data
+
+# %%
+training_data = make_dummies(training_data)
+training_data.head()
+
+# %% [markdown]
+# #### Handling Imbalanced Data
+
+# %%
+X = training_data[["cleaned_post"]]
+y = training_data.drop(columns=["cleaned_post"])
+
+# %%
+def show_distribution(data, x=["E-I", "N-S", "F-T","J-P"], fig_size=(16,4), xticks_size=10, palette="husl"):
+    fig, ax = plt.subplots(len(x), figsize=fig_size)
+    j = 0
+    for _x in x:
+        plt.subplot(1,4, j+1)
+        sns.countplot(x=_x, data=data, palette=palette)
+        plt.xticks(size=xticks_size)
+        j+=1    
+        
+show_distribution(data)
+
+# %%
+from imblearn.over_sampling import RandomOverSampler
+
+# %%
+oversample = RandomOverSampler()
+
+# %%
+y_ei = y["type_E"]
+y_ns = y["type_N"]
+y_ft = y["type_F"]
+y_jp = y["type_J"]
+
+# %%
+X_over_ei, y_over_ei = oversample.fit_resample(X, y_ei)
+X_over_ns, y_over_ns = oversample.fit_resample(X, y_ns)
+X_over_ft, y_over_ft = oversample.fit_resample(X, y_ft)
+X_over_jp, y_over_jp = oversample.fit_resample(X, y_jp)
+
+# %%
+show_class_distribution(data=X_over_ei, x=y_over_ei, figsize=(7,3), title="E-I")
+
+# %%
+show_class_distribution(data=X_over_ns, x=y_over_ns, figsize=(7,3), title="N-S")
+
+# %%
+show_class_distribution(data=X_over_ft, x=y_over_ft, figsize=(7,3), title="F-T")
+
+# %%
+show_class_distribution(data=X_over_jp, x=y_over_jp, figsize=(7,3), title="J-P")
+
+# %% [markdown]
+# #### Train-test split for each classes
+
+# %%
+from sklearn.model_selection import train_test_split
+
+# %%
+X_train_ei, X_test_ei, y_train_ei, y_test_ei = train_test_split(X_over_ei, y_over_ei, test_size=0.3, random_state=42)
+X_train_ns, X_test_ns, y_train_ns, y_test_ns = train_test_split(X_over_ns, y_over_ns, test_size=0.3, random_state=42)
+X_train_ft, X_test_ft, y_train_ft, y_test_ft = train_test_split(X_over_ft, y_over_ft, test_size=0.3, random_state=42)
+X_train_jp, X_test_jp, y_train_jp, y_test_jp = train_test_split(X_over_jp, y_over_jp, test_size=0.3, random_state=42)
+
+# %%
+X_train_ei = X_train_ei['cleaned_post']
+X_train_ns = X_train_ns['cleaned_post']
+X_train_ft = X_train_ft['cleaned_post']
+X_train_jp = X_train_jp['cleaned_post']
+
+# %%
+X_test_ei = X_test_ei['cleaned_post']
+X_test_ns = X_test_ns['cleaned_post']
+X_test_ft = X_test_ft['cleaned_post']
+X_test_jp = X_test_jp['cleaned_post']
+
+# %%
+y_train_ei.name, y_test_ei.name = "E-I", "E-I"
+y_train_ns.name, y_test_ns.name = "N-S", "N-S"
+y_train_ft.name, y_test_ft.name = "F-T", "F-T"
+y_train_jp.name, y_test_jp.name = "J-P", "J-P"
+
+# %%
+y_all_train = [y_train_ei, y_train_ns, y_train_ft,  y_train_jp]
+y_all_test  = [y_test_ei, y_test_ns, y_test_ft, y_test_jp] 
+
+# %% [markdown]
+# ### TF-IDF Vectorizer
+
+# %%
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+# %%
+vectorizer = TfidfVectorizer(max_features=10000)
+
+# %%
+vectorizer.fit(X_train_ei)
+
+# %%
+X_train_ei = vectorizer.transform(X_train_ei)
+X_test_ei  = vectorizer.transform(X_test_ei)
+
+X_train_ns = vectorizer.transform(X_train_ns)
+X_test_ns  = vectorizer.transform(X_test_ns)
+
+X_train_ft = vectorizer.transform(X_train_ft)
+X_test_ft  = vectorizer.transform(X_test_ft)
+
+X_train_jp = vectorizer.transform(X_train_jp)
+X_test_jp  = vectorizer.transform(X_test_jp)
+
+# %%
+x_all_train = [X_train_ei, X_train_ns, X_train_ft, X_train_jp]
+x_all_test  = [X_test_ei, X_test_ns, X_test_ft, X_test_jp]
+
+# %%
+tf_idf = pd.DataFrame(X_test_ei.toarray(), columns=vectorizer.get_feature_names_out())
+tf_idf.head(10)
+
+# %% [markdown]
+# ## Model Creation & Model Training & Model Saving
+
+# %%
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+import xgboost
+import pickle
+from sklearn import metrics
+
+# %%
+def create_models():
+    nb_clf  = MultinomialNB(alpha=0.01)
+    svm_clf = SVC(C=1.0, kernel='linear', degree=3, gamma='auto')
+    dt_clf  = DecisionTreeClassifier(max_depth=7)
+    rf_clf  = RandomForestClassifier(n_estimators=750)
+    xgb_clf = xgboost.XGBClassifier(use_label_encoder=False)
+    return {"NaiveBayes":nb_clf, "SVM":svm_clf, "DecisionTree":dt_clf, "RandomForest":rf_clf, "Xgboost":xgb_clf}
+
+# %% [markdown]
+# #### Model Performance Evaluation with accuracy & f1-score & roc-auc score
+
+# %%
+_metrics = ["Accuracy", "Accuracy", "Accuracy" ,"Accuracy", "Precision", "Precision", "Precision","Precision", "Recall","Recall","Recall","Recall", "F1-Score", "F1-Score", "F1-Score", "F1-Score", "Roc-Auc Score", "Roc-Auc Score", "Roc-Auc Score", "Roc-Auc Score"]
+_types   = ["E-I", "N-S", "F-T", "J-P", "E-I", "N-S", "F-T", "J-P", "E-I", "N-S", "F-T", "J-P","E-I", "N-S", "F-T", "J-P","E-I", "N-S", "F-T", "J-P"]
+_columns = ["NaiveBayes", "SVM", "DecisionTree", "RandomForest", "Xgboost"]
+
+# %%
+evaluation_df = pd.DataFrame(columns=_columns, index=[_metrics, _types])
+evaluation_df
+
+# %%
+models = create_models()
+models
+
+# %%
+for model_item in models.items():
+    for X_train, X_test, y_train, y_test in zip(x_all_train, x_all_test, y_all_train, y_all_test):
+        # Model creation and prediction
+        model = model_item[1]
+        print(f"{model} is training for {y_train.name}...")
+        model.fit(X_train, y_train)
+        pred = model.predict(X_test)
+        # Performance evaluation metrics
+        evaluation_df.loc["Accuracy",y_train.name][model_item[0]]      = round(metrics.accuracy_score(y_test, pred), 3)
+        evaluation_df.loc["Precision",y_train.name][model_item[0]]     = round(metrics.precision_score(y_test, pred), 3)
+        evaluation_df.loc["Recall",y_train.name][model_item[0]]        = round(metrics.recall_score(y_test, pred), 3)        
+        evaluation_df.loc["F1-Score",y_train.name][model_item[0]]      = round(metrics.f1_score(y_test, pred), 3)
+        evaluation_df.loc["Roc-Auc Score",y_train.name][model_item[0]] = round(metrics.roc_auc_score(y_test, pred), 3)        
+        # Save model
+        filename = f'{MODEL}{model_item[0]}_{y_test.name}.sav'
+        print(filename)
+        pickle.dump(model,open(filename, 'wb'))
+
+# %%
+evaluation_df
+
+# %% [markdown]
+# ### Save Tf-Idf Vectorizer
+
+# %%
+filename = 'vectorizer.pkl'
+pickle.dump(vectorizer,open(filename, 'wb'))
+
+
